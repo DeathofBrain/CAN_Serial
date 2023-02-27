1520a3c60d87d88ec7734c4d02bee5984174327c
DeathofBrain <l1015178747@outlook.com>
2023-02-27 14:05:23 +0800

    1

--- src/test.cpp
+++ src/test.cpp
@@ -6,9 +6,7 @@ using namespace can;
 int main(int argc, char const *argv[])
 {
     can_bus cb("can0");
-    int a;
-    std::cin>>a;
-    std::this_thread::sleep_for(std::chrono::seconds(3));
+    std::cin.get();
     cb.read();
 
 }
